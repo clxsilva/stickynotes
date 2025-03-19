1a824ce5e0ee72cc95701126963c597d46788251
CLAUDIO HENRIQUE ROCHA DA SILVA <claudio.hrsilva@senacsp.edu.br>
2025-03-18 22:14:21 -0300

    cad att

---
+++ src/views/renderer.js
@@ -0,0 +1,30 @@
+/**
+ * Processo de renderização do documento index.html
+ */
+
+console.log("Processo de renderização")
+// Inserção da data no rodapé
+function obterData() {
+    const data = new Date()
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    return data.toLocaleDateString('pt-BR', options)
+}
+
+document.getElementById('dataAtual').innerHTML = obterData()
+
+// Troca o ícone do banco de dados (status da conexão)
+// Uso da api do preload.js
+api.dbStatus((event, message) => {
+    // Teste de recebimento da mensagem
+    console.log(message)
+    if (message === "conectado") {
+        document.getElementById('iconeDB').src = "../public/img/dbon.png"
+    } else {
+        document.getElementById('iconeDB').src = "../public/img/dboff.png"
+    }
+})
